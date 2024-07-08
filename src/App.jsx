5956cd5aa2ff4e087cba2ca5f961bf7adc06bd77
sujit-mandal <sm.bright22@gmail.com>
2024-07-09 02:10:56 +0600

    All blogs page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Contact_Us_Form from "./Component/Contact_Us_Form";
 import Contact_Us_Map from "./Component/Contact_Us_Map";
 import Contact_Us_Accordion from "./Component/Contact_Us_Accordion";
+import Blog_Single_Blog from "./Component/Blog_Single_Blog";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Contact_Us_Form />
       <Contact_Us_Map />
       <Contact_Us_Accordion />
+      <Blog_Single_Blog />
     </div>
   );
 }
