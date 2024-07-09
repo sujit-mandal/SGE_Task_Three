2c08e28842973c9a030ead9793b80067742de134
sujit-mandal <sm.bright22@gmail.com>
2024-07-09 23:40:46 +0600

    Footer link updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import Contact_Us_Map from "./Component/Contact_Us_Map";
 import Contact_Us_Accordion from "./Component/Contact_Us_Accordion";
 import Blog_Single_Blog from "./Component/Blog_Single_Blog";
 import Latest_Blogs from "./Component/Latest_Blogs";
+import Footer from "./Component/Footer";
+import Subscribe from "./Component/Subscribe";
+import Study from "./Component/Study";
 
 function App() {
   return (
     <div>
       {/* <Contact_Us_Form />
       <Contact_Us_Map />
-      <Contact_Us_Accordion /> */}
-      <Blog_Single_Blog />
+      <Contact_Us_Accordion />
+      <Blog_Single_Blog /> */}
       <Latest_Blogs />
+      <Subscribe />
+      <Study />
+      <Footer />
     </div>
   );
 }
---
+++ src/Component/Footer.jsx
@@ -0,0 +1,291 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <div className="w-full bg-transparent mb-[-1px]">
+          <img
+            src="./assets/footerShape.png"
+            className="w-full bg-white"
+            alt=""
+          />
+        </div>
+        <div className="bg-[#081831] px-6 relative">
+          <div className="footer-1 text-center max-w-[1154px] mx-auto">
+            <h2 className="poppins-semibold text-[21px] text-white mb-4">
+              Our Students are Our Reference
+            </h2>
+            <img
+              className="mx-auto mb-[22px]"
+              src="./assets/logo2.png"
+              alt="logo"
+            />
+          </div>
+
+          <div className="footer-2 leading-[150%] text-[#CACACA] border-t border-white pt-[35px] max-w-[1154px] mx-auto mt-[35px] flex flex-col lg:flex-row lg:gap-[117px]">
+            <div className="w-[231px]">
+              <h2 className="mulish-regular text-base mb-4">
+                About Shabuj Global Education
+              </h2>
+              <p className="mulish-regular text-base">
+                Lorem ipsum dolor sit amet consectetur. Amet commodo bibendum ac
+                sem molestie. Nisi imperdiet orci nulla in. Convallis eu ligula
+                neque id varius sit lacus et. Pellentesque porta interdum morbi
+                enim ultrices egestas aliquet amet. Aliquet elit amet porttitor
+                posuere.
+              </p>
+            </div>
+            <div className="w-[177px]">
+              <h2 className="lg:mulish-bold mulish-regular text-base mb-4 mt-8 uppercase">
+                Study Destinations
+              </h2>
+              <ul className="mulish-regular text-base">
+                <li>UK</li>
+                <li>USA</li>
+                <li>Canada</li>
+                <li>Australia</li>
+                <li>Germany</li>
+                <li>New Zealand</li>
+              </ul>
+            </div>
+            <div className="w-[282px]">
+              <h2 className="lg:mulish-bold mulish-regular text-base mb-4 mt-8 uppercase">
+                Services for students
+              </h2>
+              <ul className="mulish-regular lg:text-base text-sm">
+                <li>Counselling</li>
+                <li>Test Preparation</li>
+                <li>Course, Country & University Selection</li>
+                <li>Scholarship</li>
+                <li>File Assessment</li>
+              </ul>
+              <p className="lg:mulish-bold mulish-regular text-base mt-3 uppercase">
+                Services for institutions
+              </p>
+              <p className="lg:mulish-bold mulish-regular text-base mt-3 uppercase">
+                Services for partners
+              </p>
+            </div>
+            <div className="w-[229px]">
+              <h2 className="lg:mulish-bold mulish-regular text-base mb-4 mt-8 uppercase">
+                Company
+              </h2>
+              <ul className="mulish-regular lg:text-base text-sm">
+                <li>About Us</li>
+                <li>Careers</li>
+                <li>Events</li>
+                <li>Blog</li>
+                <li>Contact Us</li>
+              </ul>
+              <p className="lg:mulish-bold mulish-regular text-base mt-9 uppercase">
+                search universities
+              </p>
+              <p className="lg:mulish-bold mulish-regular text-base mt-3 uppercase">
+                upcoming events
+              </p>
+              <p className="lg:mulish-bold mulish-regular text-base mt-3 mb-[41px] uppercase">
+                book online counselling
+              </p>
+            </div>
+            <div className="w-[229px]">
+              <div>
+                <h2 className="lg:mulish-bold mulish-regular text-base mb-4 mt-8 uppercase">
+                  Get Help
+                </h2>
+                <ul className="mulish-regular lg:text-base text-sm underline">
+                  <a
+                    href="https://www.shabujglobal.com/scholarship"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>Scholarship</li>
+                  </a>
+                  <a
+                    href="https://membership.shabujglobal.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>CSR</li>
+                  </a>
+                  <a
+                    href="https://www.nhs.uk"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>NHS</li>
+                  </a>
+                  <a
+                    href="https://www.gov.uk/government/organisations/uk-visas-and-immigration"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>UKVI</li>
+                  </a>
+                </ul>
+              </div>
+              <div>
+                <h2 className="lg:mulish-bold mulish-regular text-base mb-4 mt-8 uppercase">
+                  Useful Links
+                </h2>
+                <ul className="mulish-regular lg:text-base text-sm underline">
+                  <a
+                    href="https://www.shabujglobal.com/ChannelPartner"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>Channel Partner Career</li>
+                  </a>
+                  <a
+                    href="https://membership.shabujglobal.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>Become A Member</li>
+                  </a>
+                  <a
+                    href="https://www.shabujglobal.com/registration#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>HS Language IELTS</li>
+                  </a>
+                  <a
+                    href="https://webmail.names.co.uk/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <li>Webmail</li>
+                  </a>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="footer-3 leading-[150%] max-w-[1154px] mx-auto flex lg:flex-row flex-col lg:gap-[114px] border-t border-white">
+            <div className="lg:border-none border-b border-white pb-[25px]">
+              <div className="lg:w-[577px] lg:h-[164px] w-[309px] bg-[rgba(255,255,255,0.7)] rounded-2xl text-center mt-[27px] mb-[31px]">
+                <h2 className="poppins-semibold text-[28px] text-[#081831] leading-[39.2px] pt-[14px] pb-[30px]">
+                  UKVI Approved Test Centre for
+                </h2>
+                <div className="flex lg:flex-row flex-col justify-center items-center gap-[41px]">
+                  <img
+                    src="./assets/footer1.png"
+                    className="w-[125px] pb-8"
+                    alt=""
+                  />
+                  <img
+                    src="./assets/footer2.png"
+                    className="w-[166px] pb-8"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2 className="lg:mulish-bold mulish-regular uppercase text-base text-center text-[#CACACA] mb-6 mt-4">
+                Our offices
+              </h2>
+              <div className="lg:flex block gap-[23px]">
+                <div className="flex flex-wrap lg:flex-nowrap lg:gap-[23px] gap-4">
+                  <div>
+                    <h3 className="mulish-bold text-base text-[#5093FB] text-center lg:text-left mb-[18px]">
+                      Head Office (London)
+                    </h3>
+                    <h3 className="mulish-bold text-base text-[#5093FB]">
+                      South Asian Regional<br></br> Head Office (Dhaka)
+                    </h3>
+                    <p className="mulish-regular text-base text-[#CACACA]">
+                      Chattogram Office
+                    </p>
+                    <p className="mulish-regular text-base text-[#CACACA] mb-0">
+                      Sylhet Office
+                    </p>
+                  </div>
+                  <div>
+                    <h3 className="mulish-bold text-base text-[#5093FB]">
+                      India Office
+                    </h3>
+                    <p className="mulish-regular text-base text-[#CACACA]">
+                      Kannur
+                    </p>
+                    <p className="mulish-regular text-base text-[#CACACA]">
+                      Kochi
+                    </p>
+                    <h3 className="mulish-bold text-base text-[#5093FB] pt-[20px]">
+                      Middle East Office
+                    </h3>
+                    <p className="mulish-regular text-base text-[#CACACA] mb-[31px]">
+                      Dubai
+                    </p>
+                  </div>
+                  <div>
+                    <h3 className="mulish-bold text-base text-[#5093FB]">
+                      Nigeria Office
+                    </h3>
+                    <p className="mulish-regular text-base text-[#CACACA]">
+                      Lagos
+                    </p>
+                    <p className="mulish-regular text-base text-[#CACACA]">
+                      Abuja
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="footer-4 max-w-[1154px] mx-auto pb-6 flex flex-col lg:flex-row justify-between items-center border-t border-white">
+            <div className="flex gap-[26px] mt-[21px] lg:border-none border-b border-white lg:w-1/3 w-full pb-[11px]">
+              <a
+                href="https://www.facebook.com/ShabujGlobaleducationuk"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="./assets/facebook.png" alt="facebook" />
+              </a>
+              <a
+                href="https://www.instagram.com/shabujglobaleducation"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="./assets/instagram.png" alt="instagram" />
+              </a>
+              <a
+                href="https://www.linkedin.com/company/shabuj-global-education/mycompany"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="./assets/linkedin.png" alt="linkedin" />
+              </a>
+              <a
+                href="https://www.youtube.com/@shabujglobaleducation1568"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="./assets/youtube.png" alt="youtube" />
+              </a>
+            </div>
+            <div className="flex lg:gap-[18px] flex-col items-center lg:flex-row text-center">
+              <p className="mulish-regular text-[14px] text-[#CACACA] mt-6 lg:mt-0 px-20 lg:px-0">
+                Copyright © 2024, All Right Reserved{" "}
+                <span className="underline">Shabuj Global Education</span>
+              </p>
+              <p className="mulish-regular text-[14px] text-[#CACACA]">
+                Terms & Conditions
+              </p>
+              <p className="mulish-regular text-[14px] text-[#CACACA]">
+                Privacy Policy
+              </p>
+            </div>
+          </div>
+          <div className="absolute bottom-0 left-48 lg:block hidden">
+            <img src="./assets/vector7.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
